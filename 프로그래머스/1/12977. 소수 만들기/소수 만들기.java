2071ba117fae72a41f1eeedfee7d45b6a12eb596
woojeans7 <thddnwls71@naver.com>
2025-04-18 21:56:35 +0900

    [level 1] Title: 소수 만들기, Time: 2.53 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    int sum = nums[i] + nums[j] + nums[k];
+
+                    if (isPrime(sum)) {
+                        answer++;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    // 소수 판별 함수
+    private boolean isPrime(int num) {
+        if (num < 2) return false;
+        for (int i = 2; i <= Math.sqrt(num); i++) { 
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+}
