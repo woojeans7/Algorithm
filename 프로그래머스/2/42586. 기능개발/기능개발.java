bf757f3a20fc6939968dd0105dffe4d7255aa013
woojeans7 <thddnwls71@naver.com>
2025-04-23 16:38:27 +0900

    [level 2] Title: 기능개발, Time: 5.56 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,32 +2,32 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        Queue<Integer> progressQueue = new LinkedList<>();
-        Queue<Integer> speedQueue = new LinkedList<>();
+        Queue<Integer> q = new LinkedList<>();
+        Queue<Integer> s = new LinkedList<>();
 
         // 1. 큐에 데이터 삽입
         for (int i = 0; i < progresses.length; i++) {
-            progressQueue.offer(progresses[i]);
-            speedQueue.offer(speeds[i]);
+            q.offer(progresses[i]);
+            s.offer(speeds[i]);
         }
 
         List<Integer> result = new ArrayList<>();
 
-        while (!progressQueue.isEmpty()) {
+        while (!q.isEmpty()) {
             // 2. 하루치 진척도 누적
-            int size = progressQueue.size();
+            int size = q.size();
             for (int i = 0; i < size; i++) {
-                int currentProgress = progressQueue.poll();
-                int currentSpeed = speedQueue.poll();
-                progressQueue.offer(currentProgress + currentSpeed);
-                speedQueue.offer(currentSpeed);
+                int currentProgress = q.poll();
+                int currentSpeed = s.poll();
+                q.offer(currentProgress + currentSpeed);
+                s.offer(currentSpeed);
             }
 
             // 3. 배포 가능한 기능 체크
             int count = 0;
-            while (!progressQueue.isEmpty() && progressQueue.peek() >= 100) {
-                progressQueue.poll();
-                speedQueue.poll();
+            while (!q.isEmpty() && q.peek() >= 100) {
+                q.poll();
+                s.poll();
                 count++;
             }
 
