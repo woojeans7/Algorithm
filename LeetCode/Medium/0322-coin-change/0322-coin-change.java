ddb0aba3f8c8c2bd09197289b37ab03a7744e62e
woojeans7 <thddnwls71@naver.com>
2025-05-23 10:27:19 +0900

    0322-coin-change .java Medium 05-23-2025

--- LeetCode/Medium/0322-coin-change/0322-coin-change.java
+++ LeetCode/Medium/0322-coin-change/0322-coin-change.java
@@ -1,18 +1,32 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int max = amount + 1;
+        // BFS 최단거리 구하기
+        // 시작점 예약 (amount == 0)
+        Queue<int[]> queue = new ArrayDeque<>();
+        boolean[] visited = new boolean[amount+1]; // 11번 노드까지 체크해야 함.
+        
+        queue.offer(new int[]{0,0}); // 초기값, 사용한 동전 수 
+        visited[0] = true;
 
-        int[] dp = new int[amount + 1];
-        Arrays.fill(dp, max);
-        dp[0] = 0;
+        while(!queue.isEmpty()){
+            // 현재 노드 방문
+            int[] cur = queue.poll();
+            int cur_amount = cur[0], count = cur[1];
 
-        for(int i=0; i < dp.length; i++){
+            // if 현재 노드 == 11; return count;
+            if(cur_amount == amount) return count;
+
+            // 다음 노드 예약
             for(int coin : coins){
-                if(coin <= i){
-                    dp[i] = Math.min(dp[i], 1 + dp[i-coin]);
+                int next_amount = cur_amount + coin; // 다음 노드 구하기
+                if(next_amount >=0 && next_amount <= amount && !visited[next_amount]){
+                    queue.offer(new int[]{next_amount, count + 1});
+                    visited[next_amount] = true;
                 }
             }
+        
         }
-        return (dp[amount] != max) ? dp[amount] : -1;
+
+        return -1;
     }
 }
