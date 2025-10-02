01afda289ca8d3896676a19ad61d7981931f666c
woojeans7 <thddnwls71@naver.com>
2025-10-02 21:53:42 +0900

    [level 1] Title: 예산, Time: 0.59 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -2,17 +2,21 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] d, int budget) {
-        int answer = 0;
+
+        // 1. 정렬
         Arrays.sort(d);
-        int i = 0;
-        while(i < d.length && d[i] <= budget){
-            budget -= d[i];
-            i++;
-            answer++;
-            if (budget==0){
-                return answer;
+        
+        // System.out.println(Arrays.toString(d)); // 디버깅용
+        
+        // 2. 예산에서 부서별 금액 차감
+        int cnt = 0;
+        for(int i : d){
+            if(budget >= i){
+                budget -= i;
+                // 3. 지급한 부서 카운팅
+                cnt += 1;
             }
         }
-            return answer;
-        }
+        return cnt;
     }
+}
