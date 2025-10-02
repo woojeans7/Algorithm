3f0b0d24919707d1bbbcefb37fadc75a669ee436
woojeans7 <thddnwls71@naver.com>
2025-10-02 22:23:07 +0900

    [level 1] Title: 소수 만들기, Time: 3.19 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -1,27 +1,25 @@
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                for (int k = j + 1; k < nums.length; k++) {
-                    int sum = nums[i] + nums[j] + nums[k];
-
-                    if (isPrime(sum)) {
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i+1; j < nums.length; j++){
+                for(int k = j+1; k < nums.length; k++){
+                    // 3개의 수의 합
+                    int sum = nums[i]+nums[j]+nums[k];
+                    // 소수 판별
+                    if(isPrime(sum)){
                         answer++;
                     }
                 }
             }
         }
-
         return answer;
     }
-
-    // 소수 판별 함수
-    private boolean isPrime(int num) {
-        if (num < 2) return false;
-        for (int i = 2; i <= Math.sqrt(num); i++) { 
-            if (num % i == 0) return false;
+    
+    // 소수 판별 로직
+    private boolean isPrime(int num){
+        for(int i = 2; i <= Math.sqrt(num); i++){
+            if(num % i == 0) return false;
         }
         return true;
     }
