37f89ba0f1e02e8e4af68e4b3e60048656ed9e8a
woojeans7 <thddnwls71@naver.com>
2025-11-03 23:12:49 +0900

    0198-house-robber .java Medium 11-03-2025

--- LeetCode/Medium/0198-house-robber/0198-house-robber.java
+++ LeetCode/Medium/0198-house-robber/0198-house-robber.java
@@ -2,17 +2,23 @@ class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
 
-        int[] dp = new int[n];
+        Map<Integer, Integer> memo = new HashMap<>();
 
-        if(n == 1) return nums[0];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
+        return dp(n-1, memo, nums);
 
+    }
+    public int dp(int n, Map<Integer, Integer> memo, int[] nums){
+        memo.put(0, nums[0]);
+        if(n == 1) return nums[n];
+        if(n >= 2){
+            memo.put(1, Math.max(nums[0], nums[1]));
 
-        for(int i = 2; i < n; i++){
-            dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i]);
+            if(!memo.containsKey(n)){
+                memo.put(n, Math.max(dp(n-1, memo, nums), dp(n-2, memo, nums) + nums[n]));
+            }
         }
-        
-        return dp[n-1];
+
+        return memo.get(n);
+
     }
 }
