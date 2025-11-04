5f24083f9522e5cf05b5fbf8d53cf545a5f9f297
woojeans7 <thddnwls71@naver.com>
2025-11-04 10:42:49 +0900

    0070-climbing-stairs .java Easy 11-04-2025

--- LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.java
+++ LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,20 +1,15 @@
 class Solution {
     public int climbStairs(int n) {
-        int res = dp(n);
+        // 초기화
+        Map<Integer, Integer> dp = new HashMap<>();
+        dp.put(1,1);
+        dp.put(2,2);
 
-        return res;
-
-    }
-    public int dp(int n){
-        Map<Integer, Integer> memo = new HashMap<>();
-
-        memo.put(1,1);
-        memo.put(2,2);
-
+        // 점화식
         for(int i = 3; i <= n; i++){
-            memo.put(i, memo.get(i-1) + memo.get(i-2));
+            dp.put(i, dp.get(i-1) + dp.get(i-2));
         }
 
-        return memo.get(n);
+        return dp.get(n);
     }
 }
