aeb59c732fa8deb30cc9a903a5bfc269ce06285d
woojeans7 <thddnwls71@naver.com>
2026-03-05 20:35:17 +0900

    1431-kids-with-the-greatest-number-of-candies Easy 03-05-2026

--- LeetCode/Easy/1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
+++ LeetCode/Easy/1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -2,7 +2,10 @@ class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
         // 아이들한테 내 남은 사탕을 줬을 때, 그 아이가 가장 많은 사탕을 가지는 지 확인
         // 가장 많이 가진 사탕의 개수를 저장
-        int max = Arrays.stream(candies).max().getAsInt();
+        int max = 0;
+        for(int candy : candies){
+            max = Math.max(candy, max);
+        }
 
         List<Boolean> answer = new ArrayList<>();
 
