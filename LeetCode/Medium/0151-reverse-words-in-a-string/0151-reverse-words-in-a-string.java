ee6b8fe8ba5dc61c33c5849ecc363c8f8b31c618
woojeans7 <thddnwls71@naver.com>
2026-03-06 23:30:31 +0900

    0151-reverse-words-in-a-string Medium 03-06-2026

---
+++ LeetCode/Medium/0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String reverseWords(String s) {
+        String[] arr = s.trim().split("\\s+");
+
+        StringBuilder sb = new StringBuilder();
+
+        int len = arr.length;
+
+        for(int i = len-1; i >= 0; i--){
+            sb.append(arr[i]);
+            if(i != 0) sb.append(" ");
+        }
+        return sb.toString();
+    }
+}
