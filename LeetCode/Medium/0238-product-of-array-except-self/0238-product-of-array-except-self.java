6f027949e196302b67062af5f09032af7369f4af
woojeans7 <thddnwls71@naver.com>
2026-03-07 00:05:53 +0900

    0238-product-of-array-except-self Medium 03-07-2026

---
+++ LeetCode/Medium/0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        Arrays.fill(left, 1);
+        Arrays.fill(right, 1);
+
+        for(int i = 0; i < n; i++){
+            if(i != 0){
+                left[i] = left[i-1] * nums[i-1];
+            }
+        }
+        for (int i = n-1; i >= 0; i--) {
+            if(i != n-1){
+                right[i] = right[i+1] * nums[i+1];
+            }
+        }
+
+        int[] answer = new int[n];
+
+        for(int i = 0; i < n; i++){
+            answer[i] = left[i] * right[i];
+        }
+        return answer;
+    }
+}
