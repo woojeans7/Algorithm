3c7b8d860937d11f87920e7ee21d12d3bddc3022
woojeans7 <thddnwls71@naver.com>
2026-03-09 22:05:55 +0900

    [level 2] Title: 문자열 압축, Time: 5.40 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,32 +1,45 @@
 class Solution {
     public int solution(String s) {
-        int answer = s.length();
-        for(int i = 1; i < s.length() / 2 + 1; i++){
-            answer = Math.min(answer, compress(s, i));
-        }
-        return answer;
-    }
-    public int compress(String s, int length){
-        int len = s.length();
-        StringBuilder sb = new StringBuilder();
+        int min =  s.length();
         
-        String prev = s.substring(0, length);
-        int count = 1;
-        
-        for(int i = length; i < len; i += length){
-            String unit = s.substring(i, Math.min(i + length, len));
-            if (unit.equals(prev)) {
-                count++;
-            } else {
-                if (count > 1) sb.append(count);
-                sb.append(prev);
-                prev = unit;
-                count = 1;
+        // 압축단위를 1~n/2까지 반복
+        for(int unit = 1; unit <= s.length() / 2; unit++){
+            // 압축 결과
+            StringBuilder compressed = new StringBuilder();
+            // 처음 자른 문자
+            String prev = s.substring(0, unit);
+            int count = 1;
+            
+            // 단위만큼 반복
+            for (int j = unit; j <= s.length(); j += unit) {
+                // 현재 문자열
+                String current = getString(s, j, unit);
+                // 이전 문자와 같으면 카운트
+                if (prev.equals(current)) {
+                    count++;
+                } 
+                else {
+                    // 다른 문자가 나왔다면 숫자를 문자열에 추가하고 현재 문자로 초기화
+                    if (count >= 2) compressed.append(count);
+                    compressed.append(prev);
+                    prev = current;
+                    count = 1;
+                }
             }
+            // 마지막 문자 처리
+            if (count >= 2) compressed.append(count);
+            compressed.append(prev);
+            
+            // 압축 최소값 갱신
+            min = Math.min(min, compressed.length());
         }
-        if (count > 1) sb.append(count);
-        sb.append(prev);
-        
-        return sb.toString().length();
+        return min;
+    }
+    // 현재 문자가 범위를 넘어가는 것을 방지
+    private String getString(String s, int start, int len) {
+        if (start + len <= s.length()) {
+            return s.substring(start, start + len);
+        }
+        return s.substring(start);
     }
 }
