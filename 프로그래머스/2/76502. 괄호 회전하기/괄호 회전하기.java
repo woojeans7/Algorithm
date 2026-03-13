3bcd61a80500015f1da930ffde0834758d170f2c
woojeans7 <thddnwls71@naver.com>
2026-03-13 17:15:56 +0900

    [level 2] Title: 괄호 회전하기, Time: 20.68 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,11 +1,22 @@
 import java.util.*;
 
 class Solution {
-    boolean isValid(String s){
-        // if 올바른 괄호면 return true
+    public int solution(String s) {
+        int answer = 0;
+        String str = s + s;
+        
+        for(int i = 0; i < s.length(); i++){
+            String rotated = str.substring(i, i+s.length());
+            if(isValid(rotated)) answer++;
+        }
+            
+        return answer;
+    }
+    public boolean isValid(String s){
+        
         Deque<Character> stack = new ArrayDeque<>();
+        
         for(char c:s.toCharArray()){
-            // if(열린 괄호면) stack에서 push
             if(c == '(' || c == '{' || c == '['){
                 stack.push(c);
             }
@@ -13,28 +24,11 @@ class Solution {
                 if(stack.isEmpty()) return false;
                 char top = stack.pop();
                 
-                // 짝이 안맞으면
                 if(c == ')' && top != '(') return false;
                 if(c == '}' && top != '{') return false;
                 if(c == ']' && top != '[') return false;
             }
         }
         return stack.isEmpty();
-        
-    }
-    public int solution(String s) {
-        int answer = 0;
-        String doubleS = s + s;
-        System.out.println(doubleS);
-        
-        // 회전 시킬 때마다 isValid() 호출하고 true이면 answer++;
-        for(int i = 0; i < s.length(); i++){
-            String rotated = doubleS.substring(i,i+s.length());
-            if(isValid(rotated)) answer += 1;
-        }
-        
-        
-        
-        return answer;
     }
 }
