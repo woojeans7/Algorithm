737af58dc307e4f9876cebffc87ebf14a7fbb3bc
woojeans7 <thddnwls71@naver.com>
2026-03-18 19:48:47 +0900

    1207-unique-number-of-occurrences Easy 03-18-2026

---
+++ LeetCode/Easy/1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int n : arr){
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+
+        Set<Integer> set = new HashSet<>();
+        for(int i : map.values()){
+            set.add(i);
+        }
+
+        if(map.size() == set.size()) return true;
+
+        return false;
+    }
+}
