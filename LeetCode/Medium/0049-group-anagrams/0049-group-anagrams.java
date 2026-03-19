49998ccf166574de43bd3fa27db3f8f948091b2d
woojeans7 <thddnwls71@naver.com>
2026-03-19 23:07:25 +0900

    0049-group-anagrams Medium 03-19-2026

--- LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.java
+++ LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.java
@@ -3,37 +3,16 @@ class Solution {
         Map<String, List<String>> map = new HashMap<>();
 
         for(String str : strs){
-            String key = generateKey(str);
-            map.putIfAbsent(key, new LinkedList<>());
-            List<String> list = map.get(key);
-            list.add(str);
+            String key = stringSort(str);
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(str);
         }
-        return new ArrayList<>(map.values());
-    }
-    private String generateKey(String s){
-        char[] chars = s.toCharArray();   // 문자열을 문자 배열로
-        Arrays.sort(chars);  // 정렬
-        return new String(chars);   
-    }
-    // 숫자로 카운팅하기
-    // private String generateKey(String s){
-    //     // 알파벳 숫자만큼 생성
-    //     int[] map = new int[26];
-        
-    //     // 문자열을 배열로 변환
-    //     char[] arr = s.toCharArray();
-        
-    //     // 알파벳 개수를 카운팅
-    //     for(char c : arr){
-    //         map[c - 'a']++;
-    //     }
-    //     // 문자열 붙이기
-    //     StringBuilder sb = new StringBuilder();
-    //     for(int num : map){
-    //         sb.append(num); 
-    //         sb.append(","); // 구분자
-    //     }
 
-    //     return sb.toString();
-    // }
+        return new ArrayList<>(map.values());
+        
+    }
+    public String stringSort(String str){
+        char[] arr = str.toCharArray();
+        Arrays.sort(arr);
+        return new String(arr);
+    }
 }
