f344a8ada511c6eac951e770e64bff5edc9bcf2c
woojeans7 <thddnwls71@naver.com>
2026-03-24 17:31:09 +0900

    0735-asteroid-collision Medium 03-24-2026

--- LeetCode/Medium/0735-asteroid-collision/0735-asteroid-collision.java
+++ LeetCode/Medium/0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,36 +1,38 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        Stack<Integer> stack = new Stack<>();
-        
-        // 모든 소행성 탐색
-        for(int asteroid : asteroids) {
-            // 소행성 값이 양수인 경우(= 오른쪽으로 움직이는 소행성)
-            if(asteroid > 0) {
-                // stack에 값 추가
-                stack.push(asteroid);
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for(int next : asteroids){
+            boolean destroyed = false;
+
+            // 충돌 조건 : top이 양수(오른쪽)이고, next가 음수(왼쪽)일 때만
+            while(!stack.isEmpty() && stack.peek() > 0 && next < 0){
+                int top = stack.peek();
+
+                // 크기 비교 - top은 이미 양수
+                if(top < Math.abs(next)){
+                    stack.pop(); // top 소행성 파괴
+                }
+                else if(top == Math.abs(next)){
+                    stack.pop();
+                    destroyed = true;
+                    break;
+                }
+                else{
+                    destroyed = true;
+                    break;
+                }
             }
-            // 소행성 값이 음수인 경우(= 왼쪽으로 움직이는 소행성)
-            else {
-                // stack에 비교할 양수 값이 남아있고
-                // stack의 가장 위에 있는 소행성의 크기보다 작아질 때까지 소행성 파괴
-                while(!stack.isEmpty() && stack.peek() > 0 && stack.peek() < -asteroid) {
-                    stack.pop();
-                }
-                // 더 이상 stack에 비교할 양수 값이 남아있지 않은 경우
-                if(stack.isEmpty() || stack.peek() < 0) {
-                    // stack에 값 추가
-                    stack.push(asteroid);
-                }
-                // 비교할 두 소행성의 사이즈가 같은 경우
-                else if(stack.peek() == -asteroid) {
-                    // 두 소행성 모두 파괴
-                    stack.pop();
-                }
+
+            if (!destroyed) {
+                stack.push(next);
             }
         }
-        
-        // Array 형태로 return
-        return stack.stream().mapToInt(Integer::intValue).toArray();
-    }
+        int[] answer = new int[stack.size()];
+        for (int i = answer.length - 1; i >= 0; i--) {
+            answer[i] = stack.pop();
+        }
 
+        return answer;
+    }
 }
