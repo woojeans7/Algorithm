292a8eea06e312be1c8b48e0e2b935b8efb2ed0c
woojeans7 <thddnwls71@naver.com>
2026-03-24 18:26:11 +0900

    0394-decode-string Medium 03-24-2026

--- LeetCode/Medium/0394-decode-string/0394-decode-string.java
+++ LeetCode/Medium/0394-decode-string/0394-decode-string.java
@@ -1,29 +1,33 @@
 class Solution {
     public String decodeString(String s) {
-        Deque<StringBuilder> strStack = new ArrayDeque<>();
+        Deque<String> strStack = new ArrayDeque<>();
         Deque<Integer> numStack = new ArrayDeque<>();
-        StringBuilder cur = new StringBuilder();
+
+        StringBuilder sb = new StringBuilder();
         int num = 0;
 
-        for (char c : s.toCharArray()) {
+        for(char c : s.toCharArray()){
             if (Character.isDigit(c)) {
                 num = num * 10 + (c - '0');
-            } else if (c == '[') {
+            }
+            else if(c == '['){
+                strStack.push(sb.toString());
                 numStack.push(num);
-                strStack.push(cur);
                 num = 0;
-                cur = new StringBuilder();
-            } else if (c == ']') {
-                int repeat = numStack.pop();
-                StringBuilder prev = strStack.pop();
-                for (int i = 0; i < repeat; i++) {
-                    prev.append(cur);
-                }
-                cur = prev;
-            } else {
-                cur.append(c);
+                sb = new StringBuilder();
+            }
+            else if(c == ']'){
+                int n = numStack.pop();
+                String prev = strStack.pop();
+
+                String repeated = sb.toString().repeat(n);
+                sb = new StringBuilder(prev + repeated);
+            }
+            else{
+                sb.append(c);
             }
         }
-        return cur.toString();
+
+        return sb.toString();
     }
 }
