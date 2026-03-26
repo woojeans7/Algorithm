9adf13404680f27fb3569b13371bb1139250a235
woojeans7 <thddnwls71@naver.com>
2026-03-26 18:40:06 +0900

    0104-maximum-depth-of-binary-tree Easy 03-26-2026

--- LeetCode/Easy/0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ LeetCode/Easy/0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -1,11 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public int maxDepth(TreeNode root) {
-
         if(root == null) return 0;
-
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
-
-        return Math.max(left, right) + 1;
+        // System.out.println("now: " + root.val);
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
 }
