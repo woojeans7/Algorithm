86ecdd83b19875279e7a9a562d06efdfc213679f
woojeans7 <thddnwls71@naver.com>
2026-03-26 21:24:20 +0900

    0437-path-sum-iii Medium 03-26-2026

--- LeetCode/Medium/0437-path-sum-iii/0437-path-sum-iii.java
+++ LeetCode/Medium/0437-path-sum-iii/0437-path-sum-iii.java
@@ -1,21 +1,26 @@
 class Solution {
-
     public int pathSum(TreeNode root, int targetSum) {
-        HashMap<Long, Integer> map = new HashMap<>();
-        map.put(0L, 1); 
-        return dfs(root, 0, targetSum, map);
+        Map<Long, Integer> prefixMap = new HashMap<>();
+        prefixMap.put(0L, 1);
+        int count = dfs(root, targetSum, 0L, prefixMap);
+
+        return count;
+
     }
+    private int dfs(TreeNode node, int targetSum, long curSum, Map<Long, Integer> map){
+        if(node == null) return 0;
 
-    private int dfs(TreeNode node, long currSum, int targetSum, HashMap<Long, Integer> map) {
-        if (node == null) return 0;
-        currSum += node.val;
-        int count = map.getOrDefault(currSum - targetSum, 0);
-        map.put(currSum, map.getOrDefault(currSum, 0) + 1);
+        curSum += node.val;
+        // curSum - targetSum이 이전에 등장한 값인지 체크
+        int count = map.getOrDefault(curSum - targetSum, 0);
+        // 현재 누적합 저장
+        map.put(curSum, map.getOrDefault(curSum, 0) + 1);
 
-        count += dfs(node.left, currSum, targetSum, map);
-        count += dfs(node.right, currSum, targetSum, map);
+        count += dfs(node.left, targetSum, curSum, map);
+        count += dfs(node.right, targetSum, curSum, map);
 
-        map.put(currSum, map.get(currSum) - 1);
+        map.put(curSum, map.get(curSum) - 1); // 백트래킹
+
         return count;
     }
 }
