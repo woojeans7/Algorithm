56545823ae4302c5f3fcee20abe9a551d92f7dfc
woojeans7 <thddnwls71@naver.com>
2026-03-30 17:08:42 +0900

    0450-delete-node-in-a-bst Medium 03-30-2026

---
+++ LeetCode/Medium/0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -0,0 +1,31 @@
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root == null){
+            return root;
+        }
+
+        if(root.val > key){
+            root.left = deleteNode(root.left, key);
+        }
+        else if(root.val < key){
+            root.right = deleteNode(root.right, key);
+        }
+        else{
+            if(root.left == null && root.right == null) return null;
+            else if(root.left == null) return root.right;
+            else if(root.right == null) return root.left;
+            TreeNode temp = minNode(root.right);
+            root.val = temp.val;
+            root.right = deleteNode(root.right, temp.val);
+        }
+        return root;
+    }
+    private TreeNode minNode(TreeNode node){
+        int min = node.val;
+        while(node.left != null){
+            min = node.left.val;
+            node = node.left;
+        }
+        return node;
+    }
+}
