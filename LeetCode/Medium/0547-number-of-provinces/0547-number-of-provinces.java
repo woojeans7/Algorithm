e7814b1aa9cd234ef84a50f2b17ef21de8dc678a
woojeans7 <thddnwls71@naver.com>
2026-03-31 22:17:03 +0900

    0547-number-of-provinces Medium 03-31-2026

--- LeetCode/Medium/0547-number-of-provinces/0547-number-of-provinces.java
+++ LeetCode/Medium/0547-number-of-provinces/0547-number-of-provinces.java
@@ -7,18 +7,18 @@ class Solution {
         int count = 0;
         for(int i = 0; i < n; i++){
             if(visited[i]) continue;
-            dfs(isConnected, i, visited, n);
+            dfs(isConnected, i, visited);
             count++;
         }
 
         return count;
     }
-    private void dfs(int[][] graph, int cur, boolean[] visited, int n){
+    private void dfs(int[][] graph, int cur, boolean[] visited) {
         visited[cur] = true;
 
-        for (int i = 0; i < n; i++) {
-            if (!visited[i] && graph[cur][i] == 1) {
-                dfs(graph, i, visited, n);
+        for (int i = 0; i < graph.length; i++) {
+            if(!visited[i] && graph[cur][i] == 1) {
+                dfs(graph, i, visited);
             }
         }
     }
