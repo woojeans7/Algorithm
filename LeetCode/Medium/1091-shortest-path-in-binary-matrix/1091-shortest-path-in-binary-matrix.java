314cb7c9d9cc4276419942f4898bd6fa1ed419ab
woojeans7 <thddnwls71@naver.com>
2026-03-31 22:29:02 +0900

    1091-shortest-path-in-binary-matrix Medium 03-31-2026

--- LeetCode/Medium/1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ LeetCode/Medium/1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,53 +1,39 @@
-import java.util.*;
-
 class Solution {
-    static int n;
-    static int[][] distance;
+    public int shortestPathBinaryMatrix(int[][] grid) {
 
-    public static int shortestPathBinaryMatrix(int[][] grid) {
-        n = grid.length;
-        distance = new int[n][n];
-
-        if(grid[0][0] == 1 || grid[n-1][n-1] == 1){
-            return -1;
-        }
+        if(grid[0][0] == 1) return -1;
         
-        bfs(grid, 0,0);
-        return distance[n-1][n-1];
-    }
-
-    public static boolean isValid(int r, int c) {
-        return 0 <= r && r < n && 0 <= c && c < n;
-    }
-
-    public static void bfs(int[][] grid, int r, int c){
-        int[] dr = {-1, 1, 0, 0, -1, -1, 1, 1}; 
-        int[] dc = {0, 0, -1, 1, -1, 1, -1, 1}; 
-
-        Queue<int[]> queue = new LinkedList<>();
-
-        for(int i = 0; i < n; i++){
-            Arrays.fill(distance[i], -1); // 거리 초기화
+        int n = grid.length;
+        int[][] distance = new int[n][n];
+        for(int[] d : distance){
+            Arrays.fill(d, -1);
         }
 
-		queue.offer(new int[]{r, c});
-        distance[r][c] = 1;
-		
-        while (!queue.isEmpty()) {
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.add(new int[]{0,0});
+
+        distance[0][0] = 1;
+
+        int[] dr = {-1,1,0,0,-1,1,-1,1};
+        int[] dc = {0,0,-1,1,-1,1,1,-1};
+        while(!queue.isEmpty()){
             int[] cur = queue.poll();
-            int curRow = cur[0];
-            int curCol = cur[1];
-    
-            for (int i = 0; i < 8; i++) {
-                int nextRow = curRow + dr[i];
-                int nextCol = curCol + dc[i];
-                if (isValid(nextRow, nextCol)) {
-                    if (distance[nextRow][nextCol] == -1 && grid[nextRow][nextCol] == 0) {
-                        queue.offer(new int[]{nextRow, nextCol});
-                            distance[nextRow][nextCol] = distance[curRow][curCol] + 1;
+            int row = cur[0];
+            int col = cur[1];
+
+            for(int i = 0; i < 8; i++){
+                int nr = row + dr[i];
+                int nc = col + dc[i];
+
+                if(nr >= 0 && nr < n && nc >= 0 && nc < n){
+                    if(distance[nr][nc] == -1 && grid[nr][nc] == 0){
+                        distance[nr][nc] = distance[row][col] + 1;
+                        queue.offer(new int[]{nr, nc});
                     }
                 }
             }
         }
+
+        return distance[n-1][n-1];
     }
 }
