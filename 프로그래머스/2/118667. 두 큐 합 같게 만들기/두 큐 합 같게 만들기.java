5300effc0c6b035e447a5e326ced589c7c56a3d9
woojeans7 <thddnwls71@naver.com>
2026-04-01 18:07:11 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 51.03 ms, Memory: 162 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -2,40 +2,46 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
-        int count = 0;
-        long q1Sum = 0;
-        long q2Sum = 0;
+        int n = queue1.length;
+        long sum = 0;
+        long qSum1 = 0;
+        long qSum2 = 0;
         
         Queue<Integer> q1 = new ArrayDeque<>();
         Queue<Integer> q2 = new ArrayDeque<>();
         
-        for(int i = 0; i < queue1.length; i++){
+        for(int i = 0; i < n; i++){
+            qSum1 += queue1[i];
+            qSum2 += queue2[i];
+            sum += qSum1 + qSum2;
+            
             q1.offer(queue1[i]);
             q2.offer(queue2[i]);
-            
-            q1Sum += queue1[i]; 
-            q2Sum += queue2[i];
-        }
-
-        while(q1Sum != q2Sum){
-            if(q1Sum > q2Sum){
-                int cur = q1.poll(); 
-                q2Sum += cur;
-                q1Sum -= cur;
-                q2.offer(cur);
-                count++;
-            }
-            else if(q1Sum < q2Sum){
-                int cur = q2.poll(); 
-                q1Sum += cur;
-                q2Sum -= cur;
-                q1.offer(cur);
-                count++;
-            }
-            
-            if(count > queue1.length * 3) return -1;
         }
         
-        return count;
+        int count = 0;
+        long target = sum / 2;
+        int answer = 0;
+        while(qSum1 != qSum2 && count < n*3){
+            if(qSum1 > qSum2){
+                int cur = q1.poll();
+                q2.offer(cur);
+                qSum1 -= cur;
+                qSum2 += cur;
+                answer++;
+            }
+            else{
+                int cur = q2.poll();
+                q1.offer(cur);
+                qSum1 += cur;
+                qSum2 -= cur;
+                answer++;
+            }
+            count++;
+        }
+        
+        if(count >= n*3) return -1;
+        
+        return answer;
     }
 }
