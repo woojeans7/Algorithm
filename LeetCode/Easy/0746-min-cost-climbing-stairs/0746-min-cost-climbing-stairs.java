874e6fa583966a79fc0664fd33b2916a82ff9746
woojeans7 <thddnwls71@naver.com>
2026-04-03 19:38:22 +0900

    0746-min-cost-climbing-stairs Easy 04-03-2026

--- LeetCode/Easy/0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ LeetCode/Easy/0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,23 +1,16 @@
-import java.util.*;
-
 class Solution {
-    int[] memo;
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
-        memo = new int[n+1];
-        Arrays.fill(memo, -1);
-        return recursive(cost, n);
-    }
+        int[] dp = new int[n];
 
-    // n층까지 가는 최소 비용이 리턴되는 마법의 함수
-    private int recursive(int[] cost, int n){
+        dp[0] = cost[0];
+        dp[1] = cost[1];
 
-        if(n == 0 || n == 1) return 0; // 시작점이 0, 1이니까 출발할 때 비용은 0
-
-        if(memo[n] == -1){
-            memo[n] = Math.min(recursive(cost, n-1) + cost[n-1], recursive(cost, n-2) + cost[n-2]);
+        for(int i = 2; i < n; i++){
+            dp[i] = Math.min((dp[i-1] + cost[i]), (dp[i-2] + cost[i]));
         }
+
+        return Math.min(dp[n-1], dp[n-2]);
         
-        return memo[n];
     }
 }
