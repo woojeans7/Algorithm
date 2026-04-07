1b36f7aca8c3b35ffade1ef25293e84caaa76e28
woojeans7 <thddnwls71@naver.com>
2026-04-07 17:29:32 +0900

    0198-house-robber Medium 04-07-2026

--- LeetCode/Medium/0198-house-robber/0198-house-robber.java
+++ LeetCode/Medium/0198-house-robber/0198-house-robber.java
@@ -4,7 +4,7 @@ class Solution {
         if(n == 1) return nums[0];
         int[] dp = new int[n];
         dp[0] = nums[0];
-        dp[1] = nums[1];
+        dp[1] = Math.max(dp[0], nums[1]);
         for(int i = 2; i < n; i++){
             dp[i] = Math.max((dp[i-2] + nums[i]), dp[i-1]);
         }
