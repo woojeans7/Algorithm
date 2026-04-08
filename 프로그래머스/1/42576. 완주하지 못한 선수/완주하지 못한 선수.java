691c7db2c70a6b11c99bad4bc35c9594a4519a8e
woojeans7 <thddnwls71@naver.com>
2026-04-08 19:27:39 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 354.55 ms, Memory: 96.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,21 +2,19 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        Map<String, Integer> map = new HashMap<>();
-        for (int i = 0; i < participant.length; i++){
-            map.put(participant[i], map.getOrDefault(participant[i], 0) + 1);
-        }
-        for (int j =0; j < completion.length; j++){
-            map.put(completion[j], map.get(completion[j]) - 1);
+		    //✅ participant와 completion을 비내림차순으로 정렬한다.
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        
+        //✅ i를 0 ~ completion.length까지 순회한다. 
+        for (int i = 0; i < completion.length; i++) {
+        		//✅ participant와 completion이 다르면 반환한다.
+            if (!participant[i].equals(completion[i])) {
+                return participant[i];
+            }
         }
         
-        for (String key : map.keySet()) {
-        if (map.get(key) != 0) {
-            answer = key;
-            break;
-        }
-    }
-        return answer;
+        //✅ 마지막 인덱스까지 확인해도 정답이 나오지 않았다면 마지막 participant를 반환한다.
+        return participant[participant.length - 1];
     }
 }
