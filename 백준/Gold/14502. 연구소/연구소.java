d3da95a534ec1a463869b6283dc8cec1c1f9109d
woojeans7 <thddnwls71@naver.com>
2026-04-11 23:37:42 +0900

    [Gold IV] Title: 연구소, Time: 836 ms, Memory: 306144 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,107 +1,120 @@
-import java.util.*;
 import java.io.*;
-
-/*
-    BAEKJOON 14502번 연구소
-    https://www.acmicpc.net/problem/14502
-*/
+import java.util.*;
 
 public class Main {
-  public static void main(String[] args) throws IOException {
-    // 벽을 세운다
-    // 바이러스를 퍼트려본다
-    // 0의 개수를 구한다
-    // -> 이 값을 max값과 계속 비교하면서 최대값을 구한다
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine());
-
-    x = Integer.parseInt(st.nextToken());
-    y = Integer.parseInt(st.nextToken());
-
-    map = new int[x][y];
-
-    for(int i = 0; i < x; i++){
-      st = new StringTokenizer(br.readLine());
-      for(int j = 0; j < y; j++){
-        map[i][j] = Integer.parseInt(st.nextToken());
-      }
-    }
-
-    dfs(0);
-
-    System.out.println(max);
-  }
-
-  public static int x;
-  public static int y;
-  public static int[][] map;
-  public static int max = 0;
-
-  // 모든 경우의 수를 찾아야 하니까 완전탐색 재귀를 사용
-  public static void dfs(int index){
-    if(index == 3){
-      // 바이러스를 퍼트려본다.
-      // 남아있는 0의 개수를 센다
-      int ans = bfs();
-      // max = Math.max(max, 0의 개수) -> 최대값 구하기
-      max = Math.max(max, ans);
-      return;
-    }
-
-    for(int i = 0; i < x; i++){
-      for(int j = 0; j < y; j++){
-        if(map[i][j] == 0){
-          map[i][j] = 1;
-          dfs(index+1);
-          map[i][j] = 0;
+    public static void main(String[] args) throws IOException {
+        //✅ 입력 형식에 맞춰 입력값을 받는다.
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] line = reader.readLine().split(" ");
+        final int N = Integer.parseInt(line[0]);
+        final int M = Integer.parseInt(line[1]);
+        //✅ 빈 칸과 바이러스 위치를 저장한다.
+        List<Point> poison = new ArrayList<>();
+        Set<Point> wall = new HashSet<>();
+        List<Point> empty = new ArrayList<>();
+        for (int r = 0; r < N; r++) {
+            String[] s = reader.readLine().split(" ");
+            for (int c = 0; c < M; c++) {
+                switch (s[c]) {
+                    case "0": empty.add(new Point(r, c)); break;
+                    case "1": wall.add(new Point(r, c)); break;
+                    case "2": poison.add(new Point(r, c)); break;
+                }
+            }
         }
-      }
-    }
-  }
+        // BFS로 구현했지만, DFS로 구현해도 된다. 
+        // DFS(재귀)로 구현해보면 조금 복잡해지는게 생긴다.
 
-  // 바이러스를 퍼뜨리기 위한 bfs
-  public static int bfs(){
-    Queue<int[]> queue = new LinkedList<>();
-    boolean[][] visited = new boolean[x][y];
+        // BFS는 Queue<Point> queue = new ArrayDeque<>(poison)를 이용해서 
+        // 여러 바이러스를 동시에 확산시킬 수 있는데, DFS로 구현하면 DFS를 여러번 호출해야되서 번거로워진다.
 
-    int[] dr = {-1, 1, 0, 0};
-    int[] dc = {0, 0, -1, 1};
-
-    for(int i = 0; i < x; i++){
-      for(int j = 0; j < y; j++){
-        if(map[i][j] == 2){
-          queue.add(new int[] {i, j});
+        int minPoison = Integer.MAX_VALUE;
+        //✅ 임의의 3개의 빈 칸을 선택한다.
+        List<List<Point>> combList = combination(empty, 3);
+        for (List<Point> comb : combList) {
+	        //✅ 선택한 3개의 빈 칸에 벽을 세운다.
+            comb.forEach(p -> wall.add(p));
+	        //✅ 바이러스 위치를 시작으로 BFS/DFS 알고리즘을 수행한다.
+	        //✅ 안전영역의 최대 넓이를 갱신한다.
+            minPoison = Math.min(minPoison, bfs(poison, wall, N, M));
+	        //✅ 세웠던 3개의 벽을 다시 빈 칸으로 되돌린다.
+            comb.forEach(p -> wall.remove(p));
         }
-      }
+
+        System.out.println(N * M - minPoison - wall.size() - 3);
     }
 
-    while(!queue.isEmpty()){
-      int[] cur = queue.poll();
-      int r = cur[0];
-      int c = cur[1];
+    static int bfs(List<Point> poison, Set<Point> wall, int N, int M) {
+        int[] dr = {  0, -1,  0,  1 };
+        int[] dc = { -1,  0,  1,  0 };
 
-      for(int i = 0; i < 4; i++){
-        int nr = r + dr[i];
-        int nc = c + dc[i];
-        if(nr >= 0 && nr < x && nc >= 0 && nc < y){
-          if(!visited[nr][nc] && map[nr][nc] == 0){
-            queue.add(new int[] {nr, nc});
-            visited[nr][nc] = true;
-          }
+        Queue<Point> queue = new ArrayDeque<>(poison);
+        Set<Point> visited = new HashSet<>(poison);
+        int count = poison.size();
+        while (!queue.isEmpty()) {
+            Point cur = queue.remove();
+
+            for (int i = 0; i < 4; i++) {
+                Point next = new Point(cur.r + dr[i], cur.c + dc[i]);
+                if (next.inRange(N, M) && !visited.contains(next) && !wall.contains(next)) {
+                    count++;
+                    queue.add(next);
+                    visited.add(next);
+                }
+            }
         }
-      }
+        return count;
     }
 
-    // 안전 영역의 갯수 구하기
-    // 0이면서 바이러스가 들리지 않은 곳
-    int count = 0;
-    for(int i = 0; i < x; i++){
-      for(int j = 0; j < y; j++){
-        if(map[i][j] == 0 && !visited[i][j]){
-          count++;
-        }
-      }
+    static List<List<Point>> combination(List<Point> points, int count) {
+        List<List<Point>> result = new ArrayList<>();
+        combination_inner(points, new ArrayList<>(), result, count, 0);
+        return result;
     }
-    return count;
-  }
+
+    static void combination_inner(List<Point> points, List<Point> pick, List<List<Point>> result, int count, int start) {
+        if (pick.size() == count) {
+            result.add(new ArrayList<>(pick));
+            return;
+        }
+
+        for (int i = start; i < points.size(); i++) {
+            pick.add(points.get(i));
+            combination_inner(points, pick, result, count, i + 1);
+            pick.remove(pick.size() - 1);
+        }
+    }
+
+    static class Point {
+        int r;
+        int c;
+
+        public Point(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+
+        public boolean inRange(int n, int m) {
+            return r >= 0 && r < n && c >= 0 && c < m;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o instanceof Point) {
+                Point p = (Point)o;
+                return this.r == p.r && this.c == p.c;
+            }
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(r, c);
+        }
+
+        @Override
+        public String toString() {
+            return "(" + r + "," + c + ")";
+        }
+    }
 }
