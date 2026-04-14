b00acb4ee10c5a1a070aa0cb056dd78af94f9c3c
woojeans7 <thddnwls71@naver.com>
2026-04-14 15:13:06 +0900

    [level 2] Title: 피로도, Time: 3.10 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -3,31 +3,21 @@ class Solution {
     
     public int solution(int k, int[][] dungeons) {
         
-        
-        // 방문 배열 초기화
         boolean[] visited = new boolean[dungeons.length];
-        
-        // 탐색 시작
-        backtrack(k, dungeons, 0, visited);
+        backtrack(k, dungeons, visited, 0);
         
         return answer;
     }
-    private void backtrack(int k, int[][] dungeons, int cnt, boolean[] visited){
+    private void backtrack(int cur, int[][] dungeons, boolean[] visited, int cnt){
         
-        // 1. 최대 던전 수 갱신
         answer = Math.max(answer, cnt);
         
-        // 2. 던전 탐색
         for(int i = 0; i < dungeons.length; i++){
-            int minP = dungeons[i][0]; // 최소 필요 피로도
-            int useP = dungeons[i][1]; // 소모 필요도
-            
-            if(!visited[i] && k >= minP){
-                visited[i] = true; // 방문표시
-                backtrack(k - useP, dungeons, cnt + 1, visited);
-                visited[i] = false; // 백트래킹
+            if(!visited[i] && cur >= dungeons[i][0]){
+                visited[i] = true;
+                backtrack(cur-dungeons[i][1], dungeons, visited, cnt+1);
+                visited[i] = false;
             }
-            
         }
     }
 }
