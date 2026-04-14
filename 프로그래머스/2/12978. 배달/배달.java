900d02593cfd01c1e41cab83c7a15fbc22d6e859
woojeans7 <thddnwls71@naver.com>
2026-04-14 14:57:55 +0900

    [level 2] Title: 배달, Time: 8.08 ms, Memory: 96.1 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,76 +1,57 @@
 import java.util.*;
 
-class Solution {
+class Edge{
+    int to;
+    int dist;
     
-    class Edge{
-        public int node;
-        public int cost;
-        
-        public Edge(int node, int cost){
-            this.node = node;
-            this.cost = cost;
-        }
+    public Edge(int to, int dist){
+        this.to = to;
+        this.dist = dist;
     }
-    
-    final int INF = Integer.MAX_VALUE;
-    
+}
+class Solution {
     public int solution(int N, int[][] road, int K) {
-        
-        // 그래프 구현
+        final int INF = Integer.MAX_VALUE;
+
         List<List<Edge>> town = new ArrayList<>();
         
-        for(int i = 0; i <= N; i++){
+        for(int i=0; i <= N; i++){
             town.add(new ArrayList<>());
         }
         
         for(int[] r : road){
-            int u = r[0];
-            int v = r[1];
-            int weight = r[2];
-            
-            town.get(u).add(new Edge(v, weight));
-            town.get(v).add(new Edge(u, weight));
+            town.get(r[0]).add(new Edge(r[1], r[2]));
+            town.get(r[1]).add(new Edge(r[0], r[2]));
         }
         
-        // 다익스트라 실행
-        int[] distance = dijkstra(town, N, K);
-        
-        int answer = 0;
-        for(int i = 1; i <= N; i++){
-            // 걸리는 시간이 K시간 이하일 경우 +1
-            if(distance[i] <= K) answer++;
-        }
-        
-        
-        return answer;
-    }
-    
-    // 다익스트라 구현
-    private int[] dijkstra(List<List<Edge>> town, int n, int k){
-        
-        int[] distance = new int[n+1];
+        int[] distance = new int[N + 1];
         Arrays.fill(distance, INF);
         
-        // 시작점 예약  
-        Queue<Edge> pq = new PriorityQueue<>(Comparator.comparingInt(e -> e.cost));  
-        pq.add(new Edge(1, 0));  
-        distance[1] = 0;  
+        
+        Queue<Edge> pq = new PriorityQueue<>((a,b) -> Integer.compare(a.dist, b.dist));
+        pq.add(new Edge(1, 0));
+        distance[1] = 0;
         
         while(!pq.isEmpty()){
-            // 방문
-            Edge cur = pq.remove();
+            Edge cur = pq.poll();
             
-            if(distance[cur.node] < cur.cost) continue;
+            if(distance[cur.to] < cur.dist) continue;
             
-            for(Edge next : town.get(cur.node)){
-                int newDist = distance[cur.node] + next.cost;  
-                if (newDist < distance[next.node]) {  
-                    pq.add(new Edge(next.node, newDist));  
-                    distance[next.node] = newDist;  
+            for(Edge edge : town.get(cur.to)){
+                int newDist = cur.dist + edge.dist;
+                if(newDist < distance[edge.to]){
+                    distance[edge.to] = newDist;
+                    pq.add(new Edge(edge.to, newDist));
                 }
             }
         }
         
-        return distance;
+        int answer = 0;
+        for(int i = 1; i <= N; i++){
+            if(distance[i] <= K) answer++;
+        }
+        
+        return answer;
+        
     }
 }
