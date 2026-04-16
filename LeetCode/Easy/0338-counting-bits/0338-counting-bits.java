b375079c33da87941235818b6960fa4c006c7661
woojeans7 <thddnwls71@naver.com>
2026-04-16 17:29:47 +0900

    0338-counting-bits Easy 04-16-2026

--- LeetCode/Easy/0338-counting-bits/0338-counting-bits.java
+++ LeetCode/Easy/0338-counting-bits/0338-counting-bits.java
@@ -1,16 +1,10 @@
 class Solution {
     public int[] countBits(int n) {
         int[] dp = new int[n + 1];
-        int sub = 1;
-
-        for (int i = 1; i <= n; i++) {
-            if (sub * 2 == i) {
-                sub = i;
-            }
-
-            dp[i] = dp[i - sub] + 1;
+        for(int i = 1; i <= n; i++){
+            dp[i] = dp[i >> 1] + (i & 1);
         }
 
-        return dp;        
+        return dp;
     }
 }
