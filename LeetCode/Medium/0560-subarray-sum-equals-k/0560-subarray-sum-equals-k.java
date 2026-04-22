80a0d2af38a8e5131f3a41fab18886ff8794c5df
woojeans7 <thddnwls71@naver.com>
2026-04-22 19:44:37 +0900

    0560-subarray-sum-equals-k Medium 04-22-2026

---
+++ LeetCode/Medium/0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int sum = 0, count = 0;
+        
+        for (int num : nums) {
+            sum += num;
+            count += map.getOrDefault(sum - k, 0);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        
+        return count;
+    }
+}
