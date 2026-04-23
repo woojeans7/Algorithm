d752dcc88e236d1cd19254bf80638edb742a1edc
woojeans7 <thddnwls71@naver.com>
2026-04-23 20:08:10 +0900

    0078-subsets Medium 04-23-2026

--- LeetCode/Medium/0078-subsets/0078-subsets.java
+++ LeetCode/Medium/0078-subsets/0078-subsets.java
@@ -1,19 +1,20 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> answer = new ArrayList<>();
-        List<Integer> list = new ArrayList<>();
-
-        subset(nums, answer, list, 0);
+        dfs(nums, answer, new ArrayList<>(), 0);
         return answer;
-        
     }
-    public void subset(int[] nums, List<List<Integer>> answer, List<Integer> list, int depth){
-        answer.add(new ArrayList<>(list));
-
-        for(int i = depth; i < nums.length; i++){
-            list.add(nums[i]);
-            subset(nums,answer, list, i+1);
-            list.remove(list.size() - 1);
+    private void dfs(int[] nums, List<List<Integer>> answer, List<Integer> cur, int i){
+        if(i == nums.length){
+            answer.add(new ArrayList<>(cur));
+            return;
         }
+        else{
+            cur.add(nums[i]);
+            dfs(nums, answer, cur, i+1);
+            cur.remove(cur.size()-1);
+            dfs(nums, answer, cur, i+1);
+        }
+
     }
 }
