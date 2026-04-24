806db73ce7fddbcf93edd9db9ad5cabad39b82cc
woojeans7 <thddnwls71@naver.com>
2026-04-24 20:29:39 +0900

    0881-boats-to-save-people Medium 04-24-2026

---
+++ LeetCode/Medium/0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int low = 0;
+        int high = people.length - 1;
+        int count = 0;
+        while(low <= high){
+            if(people[low] + people[high] <= limit){
+                low++;
+            }
+            high--;
+            count++;
+        }
+
+        return count;
+
+    }
+}
