e55455c286b63bf0dc35f926e4e6b230f3e8a71d
woojeans7 <thddnwls71@naver.com>
2026-05-21 22:13:05 +0900

    [Passed] 배낭 채우기 (119ms, 13MB)

---
+++ Codetree/trail4/배낭 채우기/knapsack.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public int solution(int n, int m, int[] w, int[] v) {
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                dp[i][j] = dp[i-1][j];
+
+                if(j >= w[i-1]) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i-1][j - w[i-1]] + v[i-1]);
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] W = new int[N];
+        int[] V = new int[N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            W[i] = Integer.parseInt(st.nextToken());
+            V[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Main main = new Main();
+        System.out.println(main.solution(N, M, W, V));
+    }
+}
