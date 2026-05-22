87c8864a09d5fd415e2cbef5508dbce5132086f3
woojeans7 <thddnwls71@naver.com>
2026-05-22 13:42:57 +0900

    [Passed] 막대기 나누기 (89ms, 9MB)

---
+++ Codetree/trail4/막대기 나누기/rod-cutting.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public int solution(int n, int[] rods) {
+        int[] dp = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] = Math.max(dp[i], dp[i - j] + rods[j]);
+            }
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int[] A = new int[N + 1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Main main = new Main();
+        System.out.println(main.solution(N, A));
+    }
+}
