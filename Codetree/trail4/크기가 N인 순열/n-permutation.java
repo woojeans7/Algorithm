11e104eb8a1792333066629b016c3473575d9996
woojeans7 <thddnwls71@naver.com>
2026-06-04 16:57:16 +0900

    [Passed] 크기가 N인 순열 (1993ms, 29MB)

---
+++ Codetree/trail4/크기가 N인 순열/n-permutation.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public void solution(int n) {
+        boolean[] visited = new boolean[n+1];
+        permutation(n, visited, new ArrayList<>());
+    }
+    private void permutation(int n, boolean[] visited, List<Integer> cur){
+        if(cur.size() == n){
+            for(int num : cur){
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+
+        for(int i = 1; i <= n; i++){
+            if(!visited[i]){
+                visited[i] = true;
+                cur.add(i);
+                permutation(n, visited, cur);
+                visited[i] = false;
+                cur.remove(cur.size()-1);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+
+        Main main = new Main();
+        main.solution(N);
+    }
+}
