e04208dc51d29074db5ed363cf993b07ac0f625b
woojeans7 <thddnwls71@naver.com>
2026-06-19 00:14:28 +0900

    [level 1] Title: 삼총사, Time: 0.02 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/README.md
+++ 프로그래머스/1/131705. 삼총사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 삼총사 - 131705 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131705?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131705) 
 
 ### 성능 요약
 
-메모리: 86.4 MB, 시간: 0.04 ms
+메모리: 74.7 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 03일 20:03:49
+2026년 06월 19일 00:14:24
 
 ### 문제 설명
 
--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -1,19 +1,18 @@
 class Solution {
     public int solution(int[] number) {
         int answer = 0;
-
-        // 세 숫자의 합이 0인 경우의 수를 찾기 위해 중첩 반복문 사용
-        for (int i = 0; i < number.length - 2; i++) {
-            for (int j = i + 1; j < number.length - 1; j++) {
-                for (int k = j + 1; k < number.length; k++) {
-                    int sum = number[i] + number[j] + number[k]; // 세 숫자의 합 계산
-                    if (sum == 0) {
-                        answer++; // 합이 0이라면 카운트 증가
-                    }
+        int n = number.length;
+        
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                for(int k = j+1; k < n; k++){
+                    sum = number[i] + number[j] + number[k];
+                    if(sum == 0) answer++;
                 }
             }
         }
-
+        
         return answer;
     }
 }
