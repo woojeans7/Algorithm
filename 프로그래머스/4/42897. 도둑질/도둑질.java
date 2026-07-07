769099ccd2db5894ece895e820ce7d9c781b6565
woojeans7 <thddnwls71@naver.com>
2026-07-07 19:31:06 +0900

    [level 4] Title: 도둑질, Time: 14.65 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.java
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -1,26 +1,31 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] money) {
-        int n = money.length;
-        
-        if(n == 0) return 0;
-        if(n == 1) return money[0];
-        
-        // dp
-        int[] odd = new int[n];
-        int[] even = new int[n];
-        
-        // 첫 집을 터는 경우
-        odd[0] = money[0];
-        odd[1] = money[0];  
-        
-        // 첫 집을 털지 않는 경우
-        even[1] = money[1];
+        // 마을이 동그랗게 배치
+        // 인접한 두 집을 털면 경보가 울림
+        // 도둑이 훔칠 수 있는 돈의 최댓값을 구하는 문제
 
-        for(int i = 2; i < n; i++){
-            odd[i] = Math.max(odd[i-1], odd[i-2] + money[i]);
-            even[i] = Math.max(even[i-1], even[i-2] + money[i]);
+        int n = money.length;
+
+        if(n == 1) return money[0];
+
+        // 첫 번째 집을 무조건 포함한 경우
+        int[] include =  new int[n];
+        // 첫 번째 집을 무조건 배제한 경우
+        int[] exclude =  new int[n];
+
+        include[0] = money[0];
+        include[1] = Math.max(money[0], money[1]);
+        for(int i = 2; i <= n-2; i++){
+            include[i] = Math.max(include[i-1], include[i-2] +  money[i]);
         }
-        
-        return Math.max(odd[n-2], even[n-1]);     
+        exclude[1] = money[1];
+        exclude[2] = Math.max(money[1], money[2]);
+        for(int i = 3; i <= n-1; i++){
+            exclude[i] = Math.max(exclude[i-1], exclude[i-2] +  money[i]);
+        }
+
+        return Math.max(include[n-2], exclude[n-1]);
     }
 }
