54235d194dcdb9d69b8a2dc8d090f70a3284b956
woojeans7 <thddnwls71@naver.com>
2026-07-28 22:30:18 +0900

    [level 2] Title: 기능개발, Time: 2.57 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -3,30 +3,27 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         int n = progresses.length;
-        List<Integer> result = new ArrayList<>();
-            
-        // 큐에 남은 작업일을 저장
-        Queue<Integer> queue = new ArrayDeque<>();
+        List<Integer> answer = new ArrayList<>();
+        
+        Queue<Integer> queue = new LinkedList<>();
         for(int i = 0; i < n; i++){
-            int rest = (100 - progresses[i] + speeds[i] - 1) / speeds[i];
-            queue.offer(rest);
+            int remain = (100 - progresses[i] + speeds[i] - 1) / speeds[i];
+            queue.add(remain);
         }
         
         while(!queue.isEmpty()){
             int current = queue.poll();
-            int count = 1;
+            int cnt = 1;
             
             // 다음 작업이 현재보다 작거나 같으면 같이 배포
             while(!queue.isEmpty() && queue.peek() <= current){
                 queue.poll();
-                count++;    
+                cnt++;    
             }
             
-            result.add(count);
+            answer.add(cnt);
         }
-        
-        int[] answer = result.stream().mapToInt(Integer::intValue).toArray();
-        
-        return answer;
+                
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
