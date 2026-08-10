ee2f1100bcd2c1417d150c194c3218796776ec10
woojeans7 <thddnwls71@naver.com>
2026-08-10 23:03:47 +0900

    [level 3] Title: 가장 먼 노드, Time: 15.53 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -2,54 +2,54 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] edges) {
-        int[] distance = new int[n];
-        // 인접 리스트 초기화
-        List<List<Integer>> graph = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            graph.add(i, new ArrayList<>()); // 각 노드에 대해 빈 리스트 추가
+        int[] distance = new int[n + 1];
+
+        List<Integer>[] graph = new ArrayList[n + 1];
+
+        for (int i = 0; i < n + 1; i++) {
+            graph[i] = new ArrayList<>();
         }
 
-        // 양방향 간선 추가
         for (int[] edge : edges) {
-            int a = edge[0] -1;
-            int b = edge[1] -1;
+            int a = edge[0];
+            int b = edge[1];
 
-            graph.get(a).add(b); // a -> b 연결
-            graph.get(b).add(a); // b -> a 연결 (양방향)
+            graph[a].add(b);
+            graph[b].add(a);
         }
-        
+
         int answer = 0;
         int max = 0;
-        bfs(graph,distance, 0);
-        for(int d : distance){
-            if(d > max){
+        bfs(graph, distance, 1);
+
+        for (int i = 1; i <= n; i++) {
+            if (distance[i] > max) {
+                max = distance[i];
                 answer = 1;
-                max = d;
-            }
-            else if(d == max){
+            } else if (distance[i] == max) {
                 answer++;
             }
         }
+
         return answer;
     }
-    
-    void bfs(List<List<Integer>> graph,int[] distance, int start){
+
+    void bfs(List<Integer>[] graph, int[] distance, int start){
         Queue<Integer> q = new LinkedList<>();
-        
+
         Arrays.fill(distance, -1);
-        
-        q.add(start);
-        distance[start] = 0;   
-        
+
+        q.offer(start);
+        distance[start] = 0;
+
         while(!q.isEmpty()){
-            int current = q.remove();
-            for (int next : graph.get(current)) {
+            int current = q.poll();
+            for (int next : graph[current]) {
                 if (distance[next] == -1) {
-                    q.add(next);
+                    q.offer(next);
                     distance[next] = distance[current] + 1;
                 }
             }
         }
-        
     }
 }
