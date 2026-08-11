afbbf37fe6ca3e471e011b8527eb032406a56188
woojeans7 <thddnwls71@naver.com>
2026-08-11 23:32:02 +0900

    [level 2] Title: 괄호 회전하기, Time: 15.01 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -3,30 +3,31 @@ import java.util.*;
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        String str = s + s;
-        
-        for(int i = 0; i < s.length(); i++){
-            String rotated = str.substring(i, i+s.length());
-            if(isValid(rotated)) answer++;
+        int len = s.length();
+
+        for (int i = 0; i < len; i++) {
+            if (isValid(s, i, len)) answer++;
         }
-            
+
         return answer;
     }
-    public boolean isValid(String s){
-        
+
+    private boolean isValid(String s, int start, int len) {
         Deque<Character> stack = new ArrayDeque<>();
-        
-        for(char c:s.toCharArray()){
-            if(c == '(' || c == '{' || c == '['){
+
+        for (int i = 0; i < len; i++) {
+            // 나머지 연산으로 문자열을 잘라내지 않고 회전 효과를 냄
+            char c = s.charAt((start + i) % len);
+
+            if (c == '(' || c == '{' || c == '[') {
                 stack.push(c);
-            }
-            else{
-                if(stack.isEmpty()) return false;
+            } else {
+                if (stack.isEmpty()) return false;
                 char top = stack.pop();
-                
-                if(c == ')' && top != '(') return false;
-                if(c == '}' && top != '{') return false;
-                if(c == ']' && top != '[') return false;
+
+                if (c == ')' && top != '(') return false;
+                if (c == '}' && top != '{') return false;
+                if (c == ']' && top != '[') return false;
             }
         }
         return stack.isEmpty();
