c2e1b630c4d8613391c6223630d2b7ea1be0dd16
woojeans7 <thddnwls71@naver.com>
2026-08-12 21:07:23 +0900

    [level 2] Title: 타겟 넘버, Time: 3.63 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,17 +1,16 @@
 class Solution {
     public int solution(int[] numbers, int target) {
-        return backtrack(numbers, target, 0, 0);
+        return dfs(numbers,target, 0, 0);
     }
-    public int backtrack(int[] nums, int target, int cur, int idx){
-        // 끝까지 도착하면 종료
-        if(idx == nums.length){
-            return (cur == target) ? 1: 0;
+    private int dfs(int[] numbers, int target, int depth, int cur){
+        if(depth == numbers.length){
+            return cur == target ? 1 : 0;
         }
         
-        // 합계 구하기
-        int sum  = 0;
-        sum += backtrack(nums, target, cur+nums[idx], idx+1);
-        sum += backtrack(nums, target, cur-nums[idx], idx+1);
+        int sum = 0;
+        sum += dfs(numbers, target, depth + 1, cur + numbers[depth]);
+        sum += dfs(numbers, target, depth + 1, cur - numbers[depth]);
+        
         return sum;
     }
 }
