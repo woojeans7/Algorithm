b112c28ec7059cf01caa7b4355368b98b35fb15f
woojeans7 <thddnwls71@naver.com>
2026-08-18 16:50:47 +0900

    [level 3] Title: 합승 택시 요금, Time: 29.87 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -1,29 +1,24 @@
 import java.util.*;
 
-class Edge implements Comparable<Edge>{
+class Edge{
     int node;
     int cost;
-
-    public Edge(int node, int cost) {
+    
+    
+    public Edge(int node, int cost){
         this.node = node;
         this.cost = cost;
     }
-
-    @Override
-    public int compareTo(Edge o) {
-        return this.cost - o.cost;
-    }
 }
-
 class Solution {
-    final int INF = 1_000_000_000;
-
+    final int INF = (int) 1e9;
     public int solution(int n, int s, int a, int b, int[][] fares) {
-        Map<Integer,List<Edge>> graph = new HashMap<>();
-        for(int i = 0; i <= n; i++){
-            graph.put(i,new ArrayList<>());
-        }
 
+        List<List<Edge>> graph = new ArrayList<>();
+        for(int i = 0; i <= n; i++){
+            graph.add(new ArrayList<>());
+        }
+        
         for(int[] fare : fares){
             int u = fare[0];
             int v = fare[1];
@@ -32,39 +27,39 @@ class Solution {
             graph.get(u).add(new Edge(v,w));
             graph.get(v).add(new Edge(u,w));
         }
-
-        int[] distS = dijkstra(graph, s, n);
-        int[] distA = dijkstra(graph, a, n);
-        int[] distB = dijkstra(graph, b, n);
-
+        
+        int[] distS = dijkstra(n, s, graph);
+        int[] distA = dijkstra(n, a, graph);
+        int[] distB = dijkstra(n, b, graph);
+        
         int minCost = INF;
-        for(int i = 0; i <= n; i++){
+        for(int i = 1; i <= n; i++){
             if (distS[i] == INF || distA[i] == INF || distB[i] == INF) continue;
 
             int sum = distS[i] + distA[i] + distB[i];
             minCost = Math.min(minCost, sum);
         }
-
+        
         return minCost;
     }
-    private int[] dijkstra(Map<Integer,List<Edge>> graph, int start, int n) {
+    private int[] dijkstra(int n, int start, List<List<Edge>> graph){
         int[] distance = new int[n + 1];
-        Arrays.fill(distance,INF);
-
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        pq.offer(new Edge(start,0));
+        Arrays.fill(distance, INF);
+        
+        Queue<Edge> pq = new PriorityQueue<>(Comparator.comparingInt(e -> e.cost));
+        pq.offer(new Edge(start, 0));
         distance[start] = 0;
-
+        
         while(!pq.isEmpty()){
             Edge cur = pq.poll();
-
+            
             if(distance[cur.node] < cur.cost) continue;
-
+            
             for(Edge next : graph.get(cur.node)){
                 int dist = distance[cur.node] + next.cost;
                 if(dist < distance[next.node]){
                     distance[next.node] = dist;
-                    pq.offer(new Edge(next.node,dist));
+                    pq.offer(new Edge(next.node, dist));
                 }
             }
         }
