225bbc6ea3486dc7f6747247f6f3ba5d3aad3688
woojeans7 <thddnwls71@naver.com>
2026-08-26 17:07:28 +0900

    [level 3] Title: 단속카메라, Time: 4.17 ms, Memory: 59.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] routes) {
+
+        Arrays.sort(routes, (a, b) -> a[1] - b[1]);
+
+        int lastCamera = -30001;
+        int count = 0;
+
+        for(int[] route : routes){
+            int in = route[0];
+            int out = route[1];
+
+            if(in > lastCamera){
+                lastCamera = out;
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
