843083f43f92f32ada534ab320aaf45c67532dc4
woojeans7 <thddnwls71@naver.com>
2026-08-28 23:45:17 +0900

    [level 2] Title: 땅따먹기, Time: 14.43 ms, Memory: 91.1 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -1,24 +1,19 @@
-class Solution {
+import java.util.Arrays;
+
+public class Solution {
+
     int solution(int[][] land) {
-        int n = land.length;
-        int[][] dp = new int[n][4];
-        dp[0] = land[0];
-        
-        for(int i = 1; i < n; i++){
-            for(int j = 0; j < 4; j++){
+        for (int i = 1; i < land.length; i++) {
+            for (int j = 0; j < 4; j++) {
                 int max = 0;
-                for(int k = 0; k < 4; k++){
-                    if(k == j) continue;
-                    max = Math.max(max, dp[i-1][k]);
+                for (int k = 0; k < 4; k++) {
+                    if (j != k) max = Math.max(max, land[i - 1][k]);
                 }
-                dp[i][j] = land[i][j] + max;
+                land[i][j] += max;
             }
         }
-        int answer = 0;
-        for(int k : dp[n-1]){
-            answer = Math.max(answer, k);
-        }
-        
-        return answer;
+
+        return Arrays.stream(land[land.length - 1]).max().getAsInt();
     }
+
 }
