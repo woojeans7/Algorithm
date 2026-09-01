c6c4dd4bf0a170dc57025939596cffb1ae56cee3
woojeans7 <thddnwls71@naver.com>
2026-09-01 22:50:25 +0900

    [level 3] Title: 입국심사, Time: 37.60 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,23 +1,32 @@
-import java.util.Arrays;
+import java.util.*;
+
 class Solution {
     public long solution(int n, int[] times) {
         long answer = 0;
+
         Arrays.sort(times);
-        long left = 0;
-        long right = times[times.length-1] * (long)n; //모든 사람이 가장 느리게 심사받음
-        
-        while(left <= right) {
-            long mid = (left + right) / 2;
+        long low = 0;
+        long high = times[times.length - 1] * (long) n;
+
+        while(low <= high){
+            long mid = (low + high) / 2;
             long complete = 0;
-            for (int i = 0; i < times.length; i++)
-                complete += mid / times[i];
-            if (complete < n) // 해당 시간에는 모든 사람이 검사받을 수 없다.
-                left = mid + 1;
-            else {
-                right = mid - 1;
-                answer = mid; // 모두 검사받았으나, 더 최솟값이 있을 수 있다.
+
+            // 몫을 누적
+            for(int time : times){
+                complete += mid / time;
             }
-        }  
+
+            // n명 이상 검사할 수 있으면 더 짧은 시간 안에 할 수 있는지 탐색
+            if(complete >= n) {
+                high = mid - 1;
+                answer = mid;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+
         return answer;
     }
 }
